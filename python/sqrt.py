02faf11ed88cd17489cd7a195424c604cce84882
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-10-09 17:55:13 -0400

    Create sqrt.py

---
+++ python/sqrt.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+
+        Runtime: 11 ms (Beats 93.50%)
+        Memory: 13.3 MB (Beats 15.7%)
+        """
+
+        return int(x**0.5)
+        

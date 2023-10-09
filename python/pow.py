5aae342544724fce566458e2c826f44bc2ae3f9f
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-10-09 17:52:41 -0400

    Create pow.py

---
+++ python/pow.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+
+        Runtime: 8 ms (Beats 92.58%)
+        Memory: 13.2 MB (Beats 78.17%)
+        """
+
+        return x**n
+        

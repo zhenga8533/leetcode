077957521568c0d0495cd468e2d468e28631bf1e
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-10-09 17:51:50 -0400

    Create multiply.py

---
+++ python/multiply.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+
+        Runtime: 13 ms (Beats 81.48%)
+        Memory: 13.3 MB (Beats 82.79%)
+        """
+
+        return str(int(num1) * int(num2))
+        

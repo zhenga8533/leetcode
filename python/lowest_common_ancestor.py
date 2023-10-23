9c6053769c5f2ce15955c40d2e1cbb5a8e474462
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-10-22 22:09:56 -0400

    Create lowest_common_ancestor.py

---
+++ python/lowest_common_ancestor.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Runtime: 53 ms (Beats 97.90%)
+        # Memory: 28.7 MB (Beats 51.85%)
+
+        if root is None or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is None:
+            return right
+        elif right is None:
+            return left
+        
+        return root

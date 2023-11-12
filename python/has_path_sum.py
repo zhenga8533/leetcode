8c894c8585c37d4b96288cc88f86d0e275aec4e7
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-11-11 19:09:04 -0500

    Create has_path_sum.py

---
+++ python/has_path_sum.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # Runtime: 38 ms (Beats 95.84%)
+        # Memory: 17.6 MB (Beats 13.72%)
+        
+        if root is None:
+            return False
+        
+        if root.left is None and root.right is None:
+            return targetSum == root.val
+
+        target = targetSum - root.val
+        return self.hasPathSum(root.left, target) or self.hasPathSum(root.right, target)
+        

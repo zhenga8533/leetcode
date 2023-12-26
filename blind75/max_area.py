b26897fc538f8c57377d1c91d7afec8866680659
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2023-12-26 18:27:00 -0500

    Create max_area.py

---
+++ blind75/max_area.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        area = 0
+
+        while l < r:
+            area = max(area, (r - l) * min(height[r], height[l]))
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return area

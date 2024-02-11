24cb08fe73bd0ddf63d9890aa46e767882a190e6
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2024-02-11 13:53:06 -0500

    Create faulty_keyboard.py

---
+++ python/faulty_keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        ret = ''
+
+        for c in s:
+            if c == 'i':
+                ret = ret[::-1]
+            else:
+                ret += c
+
+        return ret
+        

ac94e7b15b866b28cb43e51442b762d3398e9fff
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2024-03-08 20:39:15 -0500

    Create get_min_common.py

---
+++ python/get_min_common.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+        n, m = len(nums1), len(nums2)
+
+        while i < n and j < m:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+
+        return -1

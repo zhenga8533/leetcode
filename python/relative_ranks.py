2f2eb9cf7f3fc3ffeaef9ee18679d5ae81e57382
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2024-05-08 11:34:58 -0400

    changed to binary search to improve runtime

--- python/relative_ranks.py
+++ python/relative_ranks.py
@@ -3,9 +3,24 @@ class Solution:
         order = score[:]
         order.sort(reverse=True)
 
+        def find(arr, i):
+            l = 0
+            r = len(arr) - 1
+            
+            while l <= r:
+                m = (l + r) // 2
+                if arr[m] == i:
+                    return m
+                elif arr[m] < i:
+                    r = m - 1
+                else:
+                    l = m + 1
+            
+            return -1
+
         placement = []
         for i in score:
-            index = order.index(i)
+            index = find(order, i)
             placement.append(['Gold Medal', 'Silver Medal', 'Bronze Medal'][index] \
                 if index < 3 else str(index + 1))
 

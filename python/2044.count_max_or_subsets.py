b07030772eb6bc4cebac3adb1fc8ddfb5be48994
Allen Zheng <48600472+zhenga8533@users.noreply.github.com>
2024-10-17 23:17:44 -0400

    Update 2044.count_max_or_subsets.py

--- python/2044.count_max_or_subsets.py
+++ python/2044.count_max_or_subsets.py
@@ -1,1 +1,20 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        def backtrack(index: int, curr: int) -> None:
+            if index == len(nums):
+                if curr == maximum:
+                    self.count += 1
+                return
+            
+            backtrack(index + 1, curr | nums[index])
+            backtrack(index + 1, curr)
+        
+        maximum = 0
+        for num in nums:
+            maximum |= num
 
+        self.count = 0
+        backtrack(0, 0)
+        
+        return self.count
+        
